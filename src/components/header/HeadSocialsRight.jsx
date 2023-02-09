734644e48fb139fde519996582288dd384e82fc6
Rahul_Singh <koderrahul@gmail.com>
2023-02-09 09:12:32 +0530

    new addition

--- src/components/header/HeadSocialsRight.jsx
+++ src/components/header/HeadSocialsRight.jsx
@@ -9,7 +9,7 @@ import {SiHashnode} from 'react-icons/si'
 const HeaderSocialsRight = () => {
   return (
     <div className='header__socialsright'>
-        <a href="https://twitter.com/nehathecoder" target="_blank" rel="noreferrer"><SiHashnode/></a>
+        <a href="https://rahul3526.hashnode.dev/connect-metamask-wallet-in-under-100-secs-in-your-nextjs-dapp" target="_blank" rel="noreferrer"><SiHashnode/></a>
         <a href="https://www.linkedin.com/in/rahul-singh-303071224/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
         <a href="https://linktr.ee/rahulsingh3526" target="_blank" rel="noreferrer"><HiExternalLink size ="26px"/></a>
         
